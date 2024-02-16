a9147ae454255d5bcd42843d61dea97b089abe3d
hfred0 <160284400+hfred0@users.noreply.github.com>
2024-02-16 22:50:50 +0100

    Add files via upload

---
+++ Knopf.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var index: int
+
+func setzeText(text:String):
+	$Label.text = text
+
+func _on_input_event(_viewport, event, _shape_idx):
+	if event:
+		if event is InputEventMouseButton and event.pressed:
+			if event.button_index == 1:
+				get_parent().wertAnheben(index)
+			elif event.button_index == 2:
+				get_parent().wertSenken(index)
+		get_parent().selectFeld = index
+
+func set_color(color):
+	$ColorRect.color = color
