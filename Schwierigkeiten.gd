32dd8eaef49b40fd44b16508fb8c63b388e86c73
hfred0 <160284400+hfred0@users.noreply.github.com>
2024-02-23 19:08:35 +0100

    Add files via upload
    Signed-off-by: hfred0 <160284400+hfred0@users.noreply.github.com>

--- Schwierigkeiten.gd
+++ Schwierigkeiten.gd
@@ -2,3 +2,8 @@ extends ItemList
 
 func _on_mouse_exited():
 	release_focus()
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == 1 and !event.pressed:
+			release_focus()
